b564fae77db335b4173c2d04681d0aaa3d1abaf3
AlbertoWang <albertowang@foxmail.com>
2020-01-28 20:50:24 +0800

    预留管理员适时修改模型接口

--- src/main/java/cn/edu/swjtu/demo/Controller/AdminController.java
+++ src/main/java/cn/edu/swjtu/demo/Controller/AdminController.java
@@ -1,5 +1,8 @@
 package cn.edu.swjtu.demo.Controller;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,35 +23,35 @@ public class AdminController {
 	AdminService adminService;
 
 	// 获取全部用户信息
-	@GetMapping(value = "getAllUsers")
+	@GetMapping(value = "/getAllUsers")
 	@ResponseBody
 	public List<UserInfo> getAllUsers() {
 		return adminService.getAllUsers();
 	}
 
 	// 按cookieid获取全部用户信息
-	@GetMapping(value = "getUserInfoByCookieid")
+	@GetMapping(value = "/getUserInfoByCookieid")
 	@ResponseBody
 	public UserInfo getUserInfo(@RequestParam(required = true, value = "cookieid") String cookieid) {
 		return adminService.getUserInfo(cookieid);
 	}
 
 	// 获取全部用户类型
-	@GetMapping(value = "getAllUserType")
+	@GetMapping(value = "/getAllUserType")
 	@ResponseBody
 	public List<UserType> getAllUserType() {
 		return adminService.getAllUserType();
 	}
 
 	// 获取全部用户权限
-	@GetMapping(value = "getAllUserPermission")
+	@GetMapping(value = "/getAllUserPermission")
 	@ResponseBody
 	public List<UserPermission> getAllUserPermission() {
 		return adminService.getAllUserPermission();
 	}
 
 	// 修改用户类型
-	@GetMapping(value = "updateUserType")
+	@GetMapping(value = "/updateUserType")
 	@ResponseBody
 	public int updateUserType(@RequestParam(required = true, value = "cookieid") String cookieid,
 			@RequestParam(required = true, value = "userTypeId") Long userTypeId) {
@@ -56,10 +59,38 @@ public class AdminController {
 	}
 
 	// 修改用户权限
-	@GetMapping(value = "updateUserPermission")
+	@GetMapping(value = "/updateUserPermission")
 	@ResponseBody
 	public int updateUserPermission(@RequestParam(required = true, value = "cookieid") String cookieid,
 			@RequestParam(required = true, value = "userPermissionId") Long userPermissionId) {
 		return adminService.updateUserPermission(cookieid, userPermissionId);
+	}
+
+	// TODO
+	// 实时修改模型
+	@GetMapping(value = "/updateRecommendOnHand")
+	@ResponseBody
+	public int updateRecommendOnHand(@RequestParam(required = false, value = "cookieid") String cookieid) {
+		String exe = "python";
+		String filePath = "";
+		String[] cmdArr = new String[] { exe, filePath, cookieid };
+		Process process;
+		try {
+			process = Runtime.getRuntime().exec(cmdArr);
+			InputStream is = process.getInputStream();
+			DataInputStream dis = new DataInputStream(is);
+			String str = dis.readLine();
+			try {
+				process.waitFor();
+				System.out.println(str);
+				return 1;
+			} catch (InterruptedException e) {
+				System.err.println(e);
+				return 0;
+			}
+		} catch (IOException e) {
+			System.err.println(e);
+			return 0;
+		}
 	}
 }
