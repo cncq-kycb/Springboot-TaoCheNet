5846d5e9c8ee0d3c1d289cc8efd7f11c6dce0286
AlbertoWang <albertowang@foxmail.com>
2020-05-18 23:34:26 +0800

    修改UserPreferView的POJO

--- src/main/java/cn/edu/swjtu/demo/Pojo/UserPreferView.java
+++ src/main/java/cn/edu/swjtu/demo/Pojo/UserPreferView.java
@@ -1,53 +1,53 @@
 package cn.edu.swjtu.demo.Pojo;
 
 public class UserPreferView {
-    private String cookieid;
+	private String cookieid;
 
-    private String brandName;
+	private String brandName;
 
-    private Integer seriesName;
+	private String seriesName;
 
-    private String colorName;
+	private String colorName;
 
-    private String classifyName;
+	private String classifyName;
 
-    public String getCookieid() {
-        return cookieid;
-    }
+	public String getCookieid() {
+		return cookieid;
+	}
 
-    public void setCookieid(String cookieid) {
-        this.cookieid = cookieid == null ? null : cookieid.trim();
-    }
+	public void setCookieid(String cookieid) {
+		this.cookieid = cookieid == null ? null : cookieid.trim();
+	}
 
-    public String getBrandName() {
-        return brandName;
-    }
+	public String getBrandName() {
+		return brandName;
+	}
 
-    public void setBrandName(String brandName) {
-        this.brandName = brandName == null ? null : brandName.trim();
-    }
+	public void setBrandName(String brandName) {
+		this.brandName = brandName == null ? null : brandName.trim();
+	}
 
-    public Integer getSeriesName() {
-        return seriesName;
-    }
+	public String getSeriesName() {
+		return seriesName;
+	}
 
-    public void setSeriesName(Integer seriesName) {
-        this.seriesName = seriesName;
-    }
+	public void setSeriesName(String seriesName) {
+		this.seriesName = seriesName;
+	}
 
-    public String getColorName() {
-        return colorName;
-    }
+	public String getColorName() {
+		return colorName;
+	}
 
-    public void setColorName(String colorName) {
-        this.colorName = colorName == null ? null : colorName.trim();
-    }
+	public void setColorName(String colorName) {
+		this.colorName = colorName == null ? null : colorName.trim();
+	}
 
-    public String getClassifyName() {
-        return classifyName;
-    }
+	public String getClassifyName() {
+		return classifyName;
+	}
 
-    public void setClassifyName(String classifyName) {
-        this.classifyName = classifyName == null ? null : classifyName.trim();
-    }
+	public void setClassifyName(String classifyName) {
+		this.classifyName = classifyName == null ? null : classifyName.trim();
+	}
 }
